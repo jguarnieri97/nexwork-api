aa512d48eebbdda294829c1c28ef6188c55bd3c8
Julián Guarnieri <jguarnieri@alumno.unlam.edu.ar>
2025-05-02 20:33:26 -0300

    Base Project

---
+++ src/main/java/ar/edu/unlam/tpi/nexwork_api/NexworkApiApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.tpi.nexwork_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexworkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unlam/tpi/nexwork_api/NexworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.tpi.nexwork_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
