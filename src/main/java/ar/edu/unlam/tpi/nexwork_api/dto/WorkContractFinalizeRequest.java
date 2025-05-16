6139962118b650c1ba45ca60a0002789c00da1cb
Ivan Maldonado <82183943+ivanmaldonado09@users.noreply.github.com>
2025-05-16 18:21:12 -0300

    Agrego tests

--- src/main/java/ar/edu/unlam/tpi/nexwork_api/dto/WorkContractFinalizeRequest.java
+++ src/main/java/ar/edu/unlam/tpi/nexwork_api/dto/WorkContractFinalizeRequest.java
@@ -1,14 +1,15 @@
 package ar.edu.unlam.tpi.nexwork_api.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class WorkContractFinalizeRequest {
+ 
     private String detail;
     private List<String> files;
-
-    // Este campo se setea automáticamente en el controller o service
     private String state;
 }
--- src/main/java/ar/edu/unlam/tpi/nexwork_api/dto/WorkContractResponse.java
+++ src/main/java/ar/edu/unlam/tpi/nexwork_api/dto/WorkContractResponse.java
@@ -2,9 +2,11 @@ package ar.edu.unlam.tpi.nexwork_api.dto;
 
 import java.util.List;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class WorkContractResponse {
     private Long id;
     private Double price;
@@ -16,5 +18,4 @@ public class WorkContractResponse {
     private Long applicantId;
     private List<Long> workers;
     private List<String> files;
-
 }
