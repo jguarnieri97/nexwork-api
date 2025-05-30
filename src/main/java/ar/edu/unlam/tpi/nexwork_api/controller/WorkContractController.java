a63f848aed7f8153ef00358f6aacb01d22fa9605
Ivan Maldonado <82183943+ivanmaldonado09@users.noreply.github.com>
2025-05-30 15:13:08 -0300

    .

--- src/main/java/ar/edu/unlam/tpi/nexwork_api/controller/WorkContractController.java
+++ src/main/java/ar/edu/unlam/tpi/nexwork_api/controller/WorkContractController.java
@@ -1,6 +1,5 @@
 package ar.edu.unlam.tpi.nexwork_api.controller;
 
-import ar.edu.unlam.tpi.nexwork_api.dto.*;
 import ar.edu.unlam.tpi.nexwork_api.dto.request.ContractsFinalizeRequest;
 import ar.edu.unlam.tpi.nexwork_api.dto.request.WorkContractCreateRequest;
 import ar.edu.unlam.tpi.nexwork_api.dto.request.WorkContractRequest;
